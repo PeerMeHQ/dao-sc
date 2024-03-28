ce3fada2b9bd0422c31f46002ce99c8b7034c510
Micha Vie <micha@vleap.io>
2024-03-28 16:47:12 +0100

    add voucher redeem endpoint

--- manager/src/events.rs
+++ manager/src/events.rs
@@ -17,4 +17,7 @@ pub trait EventsModule {
         #[indexed] virtual_amount: BigUint,
         #[indexed] bonus_factor: u8,
     );
+
+    #[event("voucher_redeemed")]
+    fn voucher_redeemed_event(&self, #[indexed] caller: ManagedAddress, #[indexed] nonce: u64);
 }
